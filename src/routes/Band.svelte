b6ea4b01f7a8ac416ae79c9fe18477fee9671343
Paul Scanlon <pauliescanlon@gmail.com>
2025-01-10 11:53:51 +0000

    feat: add intersection observer to player (#5)

--- src/routes/Band.svelte
+++ src/routes/Band.svelte
@@ -1,22 +1,40 @@
-<script>
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+
   export let type;
   export let embedUrl;
   export let title;
+
+  let root: any = null;
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const src = entry.target.getAttribute('data-src') || ''
+        entry.target.setAttribute('src', src)
+        
+        observer.disconnect();
+      }
+    });
+  });
+
+  onMount(() => {
+    observer.observe(root);
+  });
+
+  onDestroy(() => {
+    observer.disconnect();
+  });
+
 </script>
 
 
-{#if type === 'bandcamp'}
-  <iframe 
-    src={embedUrl}
-    class="w-full h-32"
-    {title}
+<iframe
+  bind:this={root}
+  data-src={embedUrl}
+  data-type={type}
+  src=''
+  class="w-full h-32"
+  {title}
   >
-  </iframe>
-{:else if type === 'soundcloud'}
-  <iframe
-    src={embedUrl}
-    class="w-full h-32"
-    {title}
-  >
-  </iframe>
-{/if}
+</iframe>
