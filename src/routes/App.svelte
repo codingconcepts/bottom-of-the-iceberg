fbba0a1d6eb3883853f2291dab8933d827e2aef4
Rob Reid <hello@robreid.io>
2025-07-31 20:58:16 +0100

    Basic sticky scroll (#10)
    * feat: Basic sticky scroll
    
    * feat: Better padding for sticky header
    
    * feat: Band shuffle and better header sticking

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -9,11 +9,18 @@
 
   const year = new Date().getFullYear();
 
+  let scrolled = false;
   let currentDepth = 0;
+
+  const handleScroll = () => {
+    scrolled = window.scrollY > 0;
+  };
+
+  window.addEventListener('scroll', handleScroll);
 </script>
 
 <main class="prose min-h-screen max-w-none bg-brand-depth">
-  <header class="w-full max-w-7xl mx-auto p-4 sm:p-8">
+  <header class="w-full max-w-7xl mx-auto p-4 sm:p-4 sm:my-8">
     <nav class="flex justify-end">
       <GitHubButton
         repoUrl="https://github.com/codingconcepts/bottom-of-the-iceberg"
@@ -22,21 +29,27 @@
     </nav>
   </header>
 
+  <!-- Sticky container for the title -->
+  <div
+    class="sticky top-0 z-10 bg-brand-depth duration-300 my-auto"
+    class:shadow="{scrolled}"
+    class:py-4="{scrolled}"
+  >
+    <h1
+      class="text-center font-display m-0 text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-brand-depth-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]"
+    >
+      Bottom of the Iceberg
+    </h1>
+    <p class="m-0 text-center font-bold text-md text-brand-depth-3">
+      A descent into the depths of extreme metal
+    </p>
+  </div>
+
   <DepthRuler bind:currentDepth />
 
   <section
     class="relative flex flex-col justify-center items-center min-h-96 px-8 sm:px-16"
   >
-    <div class="z-10">
-      <h1
-        class="text-center font-display m-0 text-5xl lg:text-7xl text-brand-depth-4 drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]"
-      >
-        Bottom of the Iceberg
-      </h1>
-      <p class="m-0 text-center font-bold text-xl text-brand-depth-3">
-        A descent into the depths of extreme metal
-      </p>
-    </div>
     <div class="absolute bottom-0 w-full">
       <div class="max-w-4xl mx-auto">
         <Iceberg />
@@ -66,3 +79,19 @@
     </Layer>
   {/each}
 </main>
+
+<style>
+  .sticky {
+    background-color: '#fff';
+    z-index: 50;
+  }
+
+  .shadow {
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  main {
+    position: relative;
+    z-index: 0;
+  }
+</style>
